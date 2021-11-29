12e6c6446e36164baec69be3b7d5d17a03717208
Antonio <antoniojema@gmail.com>
2021-11-29 16:17:03 +0100

    Minor

--- MyModules/plot/__init__.py
+++ MyModules/plot/__init__.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Optional, Tuple, List, Callable, Sequence, Union
 
-def plotFromFile(
-    file_name:str,
-    delimiter:Union[str, int, Sequence]=None,
-    cols:Tuple[int,int]=(0,1),
-    skip_header:int=0,
+# Plots a list of data points
+def plot(
+    xs:List[float],
+    ys:List[float],
     logscale:Optional[str]=None,
     func_x:Callable[[List[float]],List[float]]=lambda x: x,
     func_y:Callable[[List[float]],List[float]]=lambda x: x,
@@ -17,16 +16,14 @@ def plotFromFile(
     show:bool=True,
     **kwargs
 ) -> None:
-    data : List[List[float]] = np.genfromtxt(file_name, skip_header=skip_header,dtype=float,delimiter=delimiter)
-    
     if logscale is None:
-        plt.plot    (func_x(data[:,cols[0]]), func_y(data[:,cols[1]]), **kwargs)
+        plt.plot    (func_x(xs), func_y(ys), **kwargs)
     elif logscale == 'x':
-        plt.semilogx(func_x(data[:,cols[0]]), func_y(data[:,cols[1]]), **kwargs)
+        plt.semilogx(func_x(xs), func_y(ys), **kwargs)
     elif logscale == 'y':
-        plt.semilogy(func_x(data[:,cols[0]]), func_y(data[:,cols[1]]), **kwargs)
+        plt.semilogy(func_x(xs), func_y(ys), **kwargs)
     elif logscale == 'xy':
-        plt.loglog  (func_x(data[:,cols[0]]), func_y(data[:,cols[1]]), **kwargs)
+        plt.loglog  (func_x(xs), func_y(ys), **kwargs)
     else:
         raise Exception("logscale must be None, 'x', 'y', or 'xy'")
     
@@ -39,4 +36,15 @@ def plotFromFile(
     if show:
         plt.show()
 
-__all__:List[str] = ['plotFromFile']
+# Plots a list of data points from a file
+def plotFromFile(
+    file_name:str,
+    delimiter:Union[str, int, Sequence]=None,
+    cols:Tuple[int,int]=(0,1),
+    skip_header:int=0,
+    **kwargs
+) -> None:
+    data : List[List[float]] = np.genfromtxt(file_name, skip_header=skip_header,dtype=float,delimiter=delimiter)
+    plot(data[:,cols[0]], data[:,cols[1]], **kwargs)
+
+__all__:List[str] = ['plot','plotFromFile']
